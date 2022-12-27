fd5d1f6b4ffb09c43af9308112438d046c2e2176
qrj <myused@sina.com>
2022-12-27 18:38:54 +0800

    init dir

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/css/index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+const plugin = require('tailwindcss/plugin');
+
+const { ovrriedWithRoot10PX } = require('./bin/config/tailwind');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      ...ovrriedWithRoot10PX,
+      screens: {
+        xs: '375px',
+        // => @media (min-width: 375px) { ... }
+        sm: '640px',
+        // => @media (min-width: 640px) { ... }
+        md: '768px',
+        // => @media (min-width: 768px) { ... }
+        lg: '1024px',
+        // => @media (min-width: 1024px) { ... }
+        xl: '1280px',
+        // => @media (min-width: 1280px) { ... }
+        '2xl': '1536px',
+        // => @media (min-width: 1640px) { ... }
+        '3xl': '1640px',
+        maxxl: '1920px',
+      },
+      // 两种字体 LexendDeca 和 Lexend
+      fontFamily: {
+        // lexend: ['Lexend', ...defaultTheme.fontFamily.sans],
+        // lexendDeca: ['Lexend Deca', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: '#1E1C2C',
+      },
+    },
+  },
+
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        html: { fontSize: '10px' },
+      });
+    }),
+  ],
+};
